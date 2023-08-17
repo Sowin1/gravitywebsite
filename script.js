084e0320d2dad323c87b0a7ae622f7f2613efa9b
Timéo Meunier <Sowin@1CsjS701MQ.1CsjS701MQ>
2023-08-17 19:33:17 +0200

    Drawer 320px

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function show() {
+    document.getElementById('sidebar').classList.toggle('active');
+    statusbar.toggle('active');
+}
+
+window.addEventListener('scroll', function () {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.classList.contains('active')) {
+        sidebar.style.top = window.scrollY + 'px';
+    }
+});
+
 document.getElementById('popupButton').addEventListener('click', function () {
     document.getElementById('popupOverlay').style.display = 'flex';
 });
