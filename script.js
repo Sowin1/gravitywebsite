9ba0bd5c33b239ccd8f27c219a353cb6c4d5beda
Sowin Timéo <Sowin@1CsjS701MQ.1CsjS701MQ>
2023-09-01 21:40:05 +0200

    Responsive INDEX.HTML & WHO.HTML done + Message console/ quote in code

--- script.js
+++ script.js
@@ -10,3 +10,5 @@ document.getElementById('popupButton').addEventListener('click', function () {
 document.getElementById('closeButton').addEventListener('click', function () {
     document.getElementById('popupOverlay').style.display = 'none';
 });
+
+console.warn("Si vous inspectez ce site web, il est probable que vous soyez un développeur web. Je suis ouvert à tous les conseils, le lien GitHub du projet : https://github.com/Sowin1/gravitywebsite ");
